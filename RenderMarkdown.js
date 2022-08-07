afba57cf66babc54eaa683cb12e1fc683c387a54
Varshithvhegde <varshithvh@gmail.com>
2022-08-07 09:27:31 +0530

    Added Github theme to Markdown textarea

--- RenderMarkdown.js
+++ RenderMarkdown.js
@@ -1,8 +1,4 @@
 
-document.getElementById("renderbutton").addEventListener("click", function(){
-    render();
-  });
-
 function render() {
     var userText = document.getElementById("textarea").value;
     document.getElementById("render").innerHTML = marked.parse(userText);
