075619eeba30b2a40517f6160994e42adae2d4b8
Varshithvhegde <varshithvh@gmail.com>
2022-08-30 21:27:55 +0530

    Code Optimized

--- RenderMarkdown.js
+++ RenderMarkdown.js
@@ -4,13 +4,4 @@ function render() {
     document.getElementById("render").innerHTML = marked.parse(userText);
     // if(localStorage.getItem("fmy")!="1"){
         localStorage.setItem("md", userText);
-        
-    // }
-    // else{
-    //     localStorage.setItem("fmy", "0");
-    //     var md=localStorage.getItem("md");
-    //     document.getElementById('textarea').innerHTML=md;
-      
-        
-    // }
 }
