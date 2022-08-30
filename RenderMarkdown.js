41dca5e2b41bc5cd88e49cf9f5ccccb28de54089
Varshithvhegde <varshithvh@gmail.com>
2022-08-30 19:37:48 +0530

    Changes TextArea To read Only when Viewing in HTML

--- RenderMarkdown.js
+++ RenderMarkdown.js
@@ -2,15 +2,15 @@
 function render() {
     var userText = document.getElementById("textarea").value;
     document.getElementById("render").innerHTML = marked.parse(userText);
-    if(localStorage.getItem("fmy")!="1"){
+    // if(localStorage.getItem("fmy")!="1"){
         localStorage.setItem("md", userText);
         
-    }
-    else{
-        localStorage.setItem("fmy", "0");
-        var md=localStorage.getItem("md");
-        document.getElementById('textarea').innerHTML=md;
+    // }
+    // else{
+    //     localStorage.setItem("fmy", "0");
+    //     var md=localStorage.getItem("md");
+    //     document.getElementById('textarea').innerHTML=md;
       
         
-    }
+    // }
 }
