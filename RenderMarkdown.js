b6b7cdfbacba7f1e4be3b417147a37f483b1ea33
Varshithvhegde <varshithvh@gmail.com>
2022-08-30 21:28:04 +0530

    Code Optimized

--- RenderMarkdown.js
+++ RenderMarkdown.js
@@ -2,6 +2,5 @@
 function render() {
     var userText = document.getElementById("textarea").value;
     document.getElementById("render").innerHTML = marked.parse(userText);
-    // if(localStorage.getItem("fmy")!="1"){
         localStorage.setItem("md", userText);
 }
