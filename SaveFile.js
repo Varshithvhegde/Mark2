32ecb9fb20679389a5cf1124f994b6fae2d6ee6c
Varshithvhegde <varshithvh@gmail.com>
2022-08-30 21:30:41 +0530

    Code Optimized

--- SaveFile.js
+++ SaveFile.js
@@ -1,15 +1,15 @@
-
 function saveFile() {
     var fileName=prompt("What is the name of file?", "");
     if (fileName != null) {
         download(document.getElementById("textarea").value, fileName + ".md", "text/plain");
     }
 }
+
+// This function is from https://stackoverflow.com/questions/3665115/create-a-file-in-memory-for-user-to-download-not-through-server
 function download(data, filename, type) {
     var userFile = new Blob([data], { type: type });
-
     var a = document.createElement("a"),
-        url = URL.createObjectURL(userFile);
+    url = URL.createObjectURL(userFile);
     a.href = url;
     a.download = filename;
     document.body.appendChild(a);
